c083a83a79b51d09dc3a84d7c4d65df9287b4703
dvsoo <qpzm2480@naver.com>
2018-08-11 13:23:26 +0900

    sub_page finish

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'WeBlockX' });
 });
 
 module.exports = router;
+
+router.get('/minwon', function(req, res, next){
+  res.render('sub_live', {title: 'Minwon | WeBlockX'})
+});
