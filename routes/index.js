d883b4f75c39632a8d8e591e1e11b2bbf4eb2b4f
곽대용 <dayong7387@hanmail.net>
2018-08-19 17:23:18 +0900

    De

--- routes/index.js
+++ routes/index.js
@@ -16,13 +16,6 @@ router.get('/live', function(req, res, next){
     res.render('sub_live', {title: 'Live | WeBlockX'})
 });
 
-router.get('/token', function(req, res, next){
-    res.render('table_list', {title: '민원 참여하기 | WeBlockX'})
-});
-
-router.get('/token/detail', function(req, res, next){
-    res.render('sub_detail', {title: 'Live | WeBlockX'})
-});
 
 router.get('/alert', function(req, res, next){
     res.render('sub_share', {title: '나눔 나누기 | WeBlockX'})
